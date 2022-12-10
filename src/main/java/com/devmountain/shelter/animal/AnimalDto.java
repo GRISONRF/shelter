a7f8f00e54b81bb322016d03c75d5abb99bc556f
GRISONRF <grisonrf@gmail.com>
2022-12-10 16:43:25 -0700

    app proprieties to gitignore

--- src/main/java/com/devmountain/shelter/animal/AnimalDto.java
+++ src/main/java/com/devmountain/shelter/animal/AnimalDto.java
@@ -1,4 +1,30 @@
 package com.devmountain.shelter.animal;
 
-public class AnimalDto {
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimalDto implements Serializable   {
+
+    private Long id;
+    private String species;
+    private String name;
+    private int dob ;
+    private int intakeDate;
+    private String intakeMethod;
+    private String picture;
+    private String breed;
+    private String food;
+    private String food_amount;
+    private String availability;
+    private int adoptionDate;
+    private String ownerName;
+    private int ownerPhone;
+    private String ownerAddress;
 }

3310ab8acc1b65f8bcbf87f8a60e5d0d96f5a365
GRISONRF <grisonrf@gmail.com>
2022-12-13 13:25:36 -0700

    add constructor to accept dto - task

--- src/main/java/com/devmountain/shelter/task/Task.java
+++ src/main/java/com/devmountain/shelter/task/Task.java
@@ -18,19 +18,24 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
-
     @Column
     public String task;
-
     @Column
     public String when;
-
     //animal_id
     @ManyToOne
     @JsonBackReference
     private Animal animal;
-
     //staff_id
     @OneToOne
     private Staff staff;
+
+    private Task(TaskDto taskDto){
+        if (taskDto.getTask() != null) {
+            this.task = taskDto.getTask();
+        }
+        if (taskDto.getWhen() != null) {
+            this.when = taskDto.getWhen();
+        }
+    }
 }

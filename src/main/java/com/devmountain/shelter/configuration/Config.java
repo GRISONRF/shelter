b664388ce865119c3f5aa5489b26b5b163be2e1d
GRISONRF <grisonrf@gmail.com>
2022-12-14 18:53:45 -0700

    add spring-security-core

---
+++ src/main/java/com/devmountain/shelter/configuration/Config.java
@@ -0,0 +1,16 @@
+package com.devmountain.shelter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}

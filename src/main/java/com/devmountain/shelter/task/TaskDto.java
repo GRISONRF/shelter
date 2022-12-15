87029e6482bc739c5174eb445ac7c4d172d980e8
GRISONRF <grisonrf@gmail.com>
2022-12-14 17:09:27 -0700

    add staffdto and animaldto in taskdto

--- src/main/java/com/devmountain/shelter/task/TaskDto.java
+++ src/main/java/com/devmountain/shelter/task/TaskDto.java
@@ -1,5 +1,7 @@
 package com.devmountain.shelter.task;
 
+import com.devmountain.shelter.animal.AnimalDto;
+import com.devmountain.shelter.staff.StaffDto;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +16,9 @@ public class TaskDto implements Serializable {
     public Long id;
     public String task;
     public String when;
+
+    public StaffDto staffDto;
+    public AnimalDto animalDto;
 
     public TaskDto (Task task) {
         if (task.getId() != null){

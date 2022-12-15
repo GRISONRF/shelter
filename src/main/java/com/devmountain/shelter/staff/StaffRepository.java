fc2d05a82c56e94c4d4c338b2faaf5357fb709e5
GRISONRF <grisonrf@gmail.com>
2022-12-14 18:53:10 -0700

    create staff repository

--- src/main/java/com/devmountain/shelter/staff/StaffRepository.java
+++ src/main/java/com/devmountain/shelter/staff/StaffRepository.java
@@ -3,6 +3,9 @@ package com.devmountain.shelter.staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Long> {
+    Optional<Staff> findByEmail(String email);
 }

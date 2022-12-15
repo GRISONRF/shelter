94d963b24ccb39687997c00beedb394087ad535b
GRISONRF <grisonrf@gmail.com>
2022-12-14 18:54:05 -0700

    animalserviceimpl

--- src/main/java/com/devmountain/shelter/animal/AnimalServiceImpl.java
+++ src/main/java/com/devmountain/shelter/animal/AnimalServiceImpl.java
@@ -1,4 +1,11 @@
 package com.devmountain.shelter.animal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AnimalServiceImpl {
+
+    @Autowired
+    private AnimalRepository animalRepository;
 }

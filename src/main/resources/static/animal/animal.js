5bb866def9451f159b132547b4c766e67e91af31
GRISONRF <grisonrf@gmail.com>
2022-12-16 20:49:47 -0700

    add form to add animals

---
+++ src/main/resources/static/animal/animal.js
@@ -0,0 +1,8 @@
+
+const catConfig = {
+    baseUrl: 'http://localhost:8080/api',
+    headers: {
+        'Content-Type':'application/json'
+        }
+    }
+const addCatForm = document.getElementById('add-animal-form')

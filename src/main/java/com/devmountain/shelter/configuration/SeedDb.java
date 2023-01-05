32572cb090a55d63b493e16ec855fc6f5a31109e
GRISONRF <grisonrf@gmail.com>
2023-01-04 20:21:06 -0700

    seed task

--- src/main/java/com/devmountain/shelter/configuration/SeedDb.java
+++ src/main/java/com/devmountain/shelter/configuration/SeedDb.java
@@ -2,8 +2,12 @@ package com.devmountain.shelter.configuration;
 
 import com.devmountain.shelter.animal.Animal;
 import com.devmountain.shelter.animal.AnimalRepository;
+import com.devmountain.shelter.disposition.DispositionRepository;
+import com.devmountain.shelter.health.HealthRepository;
 import com.devmountain.shelter.staff.Staff;
 import com.devmountain.shelter.staff.StaffRepository;
+import com.devmountain.shelter.task.Task;
+import com.devmountain.shelter.task.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,7 +24,14 @@ public class SeedDb implements CommandLineRunner {
     @Autowired
     private AnimalRepository animalRepository;
     @Autowired
+    private HealthRepository healthRepository;
+    @Autowired
+    private DispositionRepository dispositionRepository;
+    @Autowired
     private StaffRepository staffRepository;
+    @Autowired
+    private TaskRepository taskRepository;
+
 
     @Override
     public void run(String ... args) throws Exception {
@@ -42,6 +53,11 @@ public class SeedDb implements CommandLineRunner {
         staff1.setAddress("Denver-CO");
         staff1.setRole("Volunteer");
         staffRepository.saveAll(Arrays.asList(staff1));
+
+        Animal animalTest = animalRepository.findById(1L).orElse(null);
+        Staff staffTest = staffRepository.findById(1L).orElse(null);
+        Task task1 = new Task(null, "Take the trash out", "Wednesday - 2pm", animalTest, staffTest);
+        taskRepository.saveAll(Arrays.asList(task1));
 
     }
 }

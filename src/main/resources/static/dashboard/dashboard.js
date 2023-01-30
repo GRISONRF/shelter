cfc67a72b75692a4aaa7c1085ae129062382d32f
GRISONRF <grisonrf@gmail.com>
2023-01-29 18:04:03 -0700

    dashboard delete task

--- src/main/resources/static/dashboard/dashboard.js
+++ src/main/resources/static/dashboard/dashboard.js
@@ -1,6 +1,49 @@
-$(document).ready(function() {
-    console.log("inside of here")
-    $('.delete-button').click(function() {
-        $(this).closest('tr').remove();
-    });
-});
+const taskConfig = {
+    baseUrl: 'http://localhost:8080/api/task',
+    headers: {'Content-Type':'application/json'}
+}
+const staffConfig = {
+    baseUrl: 'http://localhost:8080/api/staff',
+    headers: {'Content-Type':'application/json'}
+}
+
+//async function handleDeleteTask(taskId) {
+////    console.log(staffId)
+//    console.log(taskId)
+//  await fetch(`${taskConfig.baseUrl}/${taskId}`, {
+//    method: "DELETE",
+//    headers: taskConfig.headers
+//  })
+//    .then(response => response.json())
+//    .then(data => {
+//      console.log(data);
+//      // make another fetch call to retrieve updated data
+//      return fetch(`${staffConfig.baseUrl}/dashboard?staffId=${staffId}`)
+//        .then(response => response.json())
+//        .then(data => {
+//          // update the page with the updated data
+//        })
+////        .catch(err => console.error(err));
+//    })
+////    .catch(err => console.error(err));
+//}
+
+
+/*
+dashboard:
+display the dashboard info depending on which staff
+when deleting the task:
+
+call handle delete task:
+    call the task id and delete it from the database
+    call the dashboard again with the staffID
+
+
+
+*/
+
+async function handleDeleteTask(taskId) {
+    console.log(taskId);
+    const taskRow = document.querySelector(`tr[data-task-id="${taskId}"]`);
+    taskRow.remove();
+}
